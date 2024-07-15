bd791397529a23102d65e4c8ed764823947fb640
burak74 <burakalkan74@gmail.com>
2024-07-16 02:05:50 +0300

    contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import BannerImage from '../assets/banner.png';
+import '../styles/Contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+        <div className='leftSide' style={{backgroundImage: `url(${BannerImage})`}}>
+        </div>
+        <div className='rightSide'>
+            <h1>Bizimle İletişime Geçin</h1>
+            <form>
+                <label>Ad Soyad</label>
+                <input type="text" placeholder='Lütfen adınızı soyadınızı giriniz...'></input>
+                <label>E-Mailiniz</label>
+                <input type="email" placeholder='Lütfen mailinizi giriniz...'></input>
+                <label>Mesajınız</label>
+                <textarea placeholder='Lütfen mesajınızı giriniz...'></textarea>
+                <button>Gönder</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
